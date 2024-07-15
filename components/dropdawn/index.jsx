e860a75f162390b6b2fd08a2fdbec7105941b9d2
Marrie713 <marielybragil@gmail.com>
2024-07-15 14:34:44 -0300

    Primeiro commit!

--- components/dropdawn/index.jsx
+++ components/dropdawn/index.jsx
@@ -12,13 +12,9 @@ function ButtonDarkExample() {
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1" active>
-            Action
-          </Dropdown.Item>
+          <Dropdown.Item href="#/action-1" active> Action </Dropdown.Item>
           <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href="#/action-4">Separated link</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
 
